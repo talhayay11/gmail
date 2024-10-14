ba8f204337810cc2e8b11298e987242d875237e0
TALHA YAY <394762@ogr.ktu.edu.tr>
2024-10-14 17:43:05 +0300

    feat: added hover effect, action icons, and dynamic mail creation in 'Sent' section
    - Implemented hover effect on mail list items to highlight and enlarge the item when hovered.
    
    - Enabled dynamic mail creation in the 'Sent' section based on user input in the 'Compose' popup.

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const mailData = {
   social: [
     { id: 6, sender: 'Social App', subject: 'You have new friend requests!', date: '12/09/23', isRead: false },
     { id: 7, sender: 'Community Forum', subject: 'Join the latest discussions', date: '12/08/23', isRead: false }
-  ]
+  ],
+  sent: []
 };
 
 // MailList bileşeni
@@ -24,6 +25,8 @@ function MailComponent() {
   const [mails, setMails] = useState(mailData);
   const [popup, setPopup] = useState(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [toValue, setToValue] = useState(''); // To kısmı için state
+  const [subjectValue, setSubjectValue] = useState(''); // Subject kısmı için state
 
   // Aktif sekmeye göre mail listesini güncelle
   const getActiveMails = () => {
@@ -36,8 +39,30 @@ function MailComponent() {
 
   const closeComposePopup = () => {
     setIsPopupOpen(false);
+    setToValue(''); // Popup kapatıldığında inputları temizle
+    setSubjectValue('');
   };
   
+  // Mail gönderme işlemi
+  const handleSend = () => {
+    const newMail = {
+      id: mails.sent.length + 1, // Unique ID
+      sender: `receiver: ${toValue}`,
+      subject: subjectValue,
+      date: new Date().toLocaleDateString(), // Şu anki tarih
+      isRead: false
+    };
+
+    // Yeni maili sent kategorisine ekle
+    setMails(prevState => ({
+      ...prevState,
+      sent: [...prevState.sent, newMail]
+    }));
+
+    // Popup'ı kapat ve inputları temizle
+    closeComposePopup();
+  };
+
   // Tıklanan mail'i okundu olarak işaretle ve popup'ı göster
   const markAsRead = (id) => {
     const updatedMails = mails[activeTab].map(mail =>
@@ -60,7 +85,7 @@ function MailComponent() {
 
       <div className="mail__sidebar-side">
         <ul className="mail__sidebar-side-box">
-          <li className="mail__sidebar-side-box-usage">
+          <li className="mail__sidebar-side-box-usage" onClick={() => setActiveTab('primary')}>
             <i className="fa-solid fa-inbox mail__sidebar-side-box-icon"></i>
             <span className="mail__sidebar-side-box-text">Inbox</span>
           </li>
@@ -75,7 +100,7 @@ function MailComponent() {
             <span className="mail__sidebar-side-box-text">Snoozed</span>
           </li>
 
-          <li className="mail__sidebar-side-box-usage">
+          <li className="mail__sidebar-side-box-usage"  onClick={() => setActiveTab('sent')}>
             <i className="fa-regular fa-paper-plane mail__sidebar-side-box-icon"></i>
             <span className="mail__sidebar-side-box-text">Sent</span>
           </li>
@@ -92,8 +117,7 @@ function MailComponent() {
         </ul>
 
         <div className="mail__sidebar-side-down">
-          <div className="mail__sidebar-side-down-title">
-            Labels
+          <div className="mail__sidebar-side-down-title">Labels
             <i className="fa-solid fa-plus mail__sidebar-side-down-title-icon"></i>
           </div>
 
@@ -132,7 +156,7 @@ function MailComponent() {
       <div className="mail__content-top">
         <div className="mail__content-top-left">
           <div className="mail__content-top-left-select">
-            <input type="checkbox" className="mail__content-top-left-select-checkbox" />
+            <input type="checkbox" className="mail__content-top-left-select-checkbox"/>
             <i className="fa-solid fa-caret-down"></i>
           </div>
 
@@ -141,38 +165,100 @@ function MailComponent() {
         </div>
         
         <div className="mail__content-top-right">
-          <span className="mail__content-top-right-number">1-16 of 16</span>
+          <span className="mail__content-top-right-number">1-1 of 1</span>
           <i className="fa-solid fa-angle-left"></i>
           <i className="fa-solid fa-angle-right"></i>
         </div>
       </div>
 
-      {/* Tablar */}
-      <div className="mail__content-tab">
-        <button 
-          className={`mail__content-tab-primary ${activeTab === 'primary' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('primary')}
-        >
-          <i className="fa-solid fa-inbox fa-xs mail__content-tab-primary-icon"></i>
-          <span className="mail__content-tab-primary-text">Primary</span>
-        </button>
+        {/* Tablar */}
+        {/* primary, promotions, social sadece primary tabı aktifken gösterilir */}
+        {activeTab === 'primary' && (
+          <div className="mail__content-tab">
+            <button 
+              className={`mail__content-tab-primary ${activeTab === 'primary' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('primary')}
+              style={activeTab === 'primary' ? { borderBottom: '2px solid #2f80ed', color: '#2f80ed' } : {}}
+            >
+              <i className="fa-solid fa-inbox fa-xs mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Primary</span>
+            </button>
 
-        <button 
-          className={`mail__content-tab-promotions ${activeTab === 'promotions' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('promotions')}
-        >
-          <i className="fa-solid fa-tag fa-lg mail__content-tab-primary-icon"></i>
-          <span className="mail__content-tab-primary-text">Promotions</span>
-        </button>
+            <button 
+              className={`mail__content-tab-promotions ${activeTab === 'promotions' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('promotions')}
+            >
+              <i className="fa-solid fa-tag fa-lg mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Promotions</span>
+            </button>
 
-        <button 
-          className={`mail__content-tab-social ${activeTab === 'social' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('social')}
-        >
-          <i className="fa-solid fa-user-group fa-sm mail__content-tab-primary-icon"></i>
-          <span className="mail__content-tab-primary-text">Social</span>
-        </button>
-      </div>
+            <button 
+              className={`mail__content-tab-social ${activeTab === 'social' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('social')}
+            >
+              <i className="fa-solid fa-user-group fa-sm mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Social</span>
+            </button>
+          </div>
+        )}
+
+         {activeTab === 'promotions' && (
+          <div className="mail__content-tab">
+            <button 
+              className={`mail__content-tab-primary ${activeTab === 'primary' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('primary')}
+            >
+              <i className="fa-solid fa-inbox fa-xs mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Primary</span>
+            </button>
+
+            <button 
+              className={`mail__content-tab-promotions ${activeTab === 'promotions' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('promotions')}
+              style={activeTab === 'promotions' ? { borderBottom: '2px solid #2f80ed', color: '#2f80ed' } : {}}
+            >
+              <i className="fa-solid fa-tag fa-lg mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Promotions</span>
+            </button>
+
+            <button 
+              className={`mail__content-tab-social ${activeTab === 'social' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('social')}
+            >
+              <i className="fa-solid fa-user-group fa-sm mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Social</span>
+            </button>
+          </div>
+        )}
+             
+        {activeTab === 'social' && (
+          <div className="mail__content-tab">
+            <button 
+              className={`mail__content-tab-primary ${activeTab === 'primary' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('primary')}
+            >
+              <i className="fa-solid fa-inbox fa-xs mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Primary</span>
+            </button>
+
+            <button 
+              className={`mail__content-tab-promotions ${activeTab === 'promotions' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('promotions')}
+            >
+              <i className="fa-solid fa-tag fa-lg mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Promotions</span>
+            </button>
+
+            <button 
+              className={`mail__content-tab-social ${activeTab === 'social' ? 'active' : ''}`} 
+              onClick={() => setActiveTab('social')}
+              style={activeTab === 'social' ? { borderBottom: '2px solid #2f80ed', color: '#2f80ed' } : {}}
+            >
+              <i className="fa-solid fa-user-group fa-sm mail__content-tab-primary-icon"></i>
+              <span className="mail__content-tab-primary-text">Social</span>
+            </button>
+          </div>
+        )}
 
       {/* Mail listesi */}
       <div className="mail__content-list">
@@ -187,7 +273,18 @@ function MailComponent() {
             <span className="mail__content-list-item-sender">{mail.sender}</span>
             <span className="mail__content-list-item-subject">{mail.subject}</span>
             <span className="mail__content-list-item-date">{mail.date}</span>
+                {/* İkon kutusu */}
+        <div className="mail__content-list-item-left">
+          <i class="fa-solid fa-ellipsis-vertical mail__content-list-item-left-icon"></i>
           </div>
+
+          <div className="mail__content-list-item-icons">
+           <i className="fa-solid fa-box-archive mail__content-list-item-icons-icon"></i>
+           <i className="fa-solid fa-trash-can mail__content-list-item-icons-icon"></i>
+           <i className="fa-regular fa-envelope mail__content-list-item-icons-icon"></i>
+           <i className="fa-regular fa-clock mail__content-list-item-icons-icon"></i>
+          </div>
+         </div>
         ))}
 
      {isPopupOpen && (
@@ -195,7 +292,7 @@ function MailComponent() {
           <div className="mail__sidebar-compose-popup-header">
             <h4 className="mail__sidebar-compose-popup-header-text">New Message</h4>
             <div className="mail__sidebar-compose-popup-header-right">
-              <button className="mail__sidebar-compose-popup-header-button" onClick={() => alert('Minimize clicked')}>_</button>
+              <button className="mail__sidebar-compose-popup-header-button" onClick={closeComposePopup}>_</button>
               <button className="mail__sidebar-compose-popup-header-button" onClick={closeComposePopup}>X</button>
             </div>
           </div>
@@ -203,17 +300,22 @@ function MailComponent() {
           <div className="mail__sidebar-compose-popup-body">
             <div className="mail__sidebar-compose-popup-body-from">
               <label className="mail__sidebar-compose-popup-body-from-text">From</label>
-              <input className="mail__sidebar-compose-popup-body-from-input" type="text" placeholder="sender" readOnly />
+              <input className="mail__sidebar-compose-popup-body-from-input" type="text" placeholder="sender"
+              value={toValue}
+              onChange={(e) => setToValue(e.target.value)}/>
             </div>
 
             <div className="mail__sidebar-compose-popup-body-to">
               <label className="mail__sidebar-compose-popup-body-to-text">To</label>
-              <input className="mail__sidebar-compose-popup-body-to-input" type="text" placeholder="who" />
+              <input className="mail__sidebar-compose-popup-body-to-input" type="text" placeholder="who"/>
             </div>
             
             <div className="mail__sidebar-compose-popup-body-subject">
-              <label className="mail__sidebar-compose-popup-body-subject-text">Subject</label>
-              <input className="mail__sidebar-compose-popup-body-subject-input" type="text" placeholder="" />
+              <label className="mail__sidebar-compose-popup-body-subject-text"></label>
+              <input className="mail__sidebar-compose-popup-body-subject-input" type="text" placeholder="Subject"  
+               
+               value={subjectValue}
+               onChange={(e) => setSubjectValue(e.target.value)}/>
             </div>
 
             <div className="mail__sidebar-compose-popup-body-text">
@@ -222,20 +324,21 @@ function MailComponent() {
           </div>
 
           <div className="mail__sidebar-compose-popup-footer">
-            <button className="mail__sidebar-compose-popup-footer-button">Send
-            <button class="fa-solid fa-caret-down fa-xs mail__sidebar-compose-popup-footer-button-icon"></button>
-            </button>
+          <div className="mail__sidebar-compose-popup-footer-button" onClick={handleSend}>
+          <span  className="mail__sidebar-compose-popup-footer-button-text">Send</span>
+          <button className="fa-solid fa-caret-down fa-xs mail__sidebar-compose-popup-footer-button-icon"></button>
+            </div>
 
             <div className="mail__sidebar-compose-popup-footer-icons">
               <i className="fa fa-paperclip fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
               <i className="fa fa-image fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
               <i className="fa fa-smile fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-brands fa-google-drive fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-regular fa-image fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-solid fa-lock fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-solid fa-pen fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-solid fa-ellipsis-vertical fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
-              <i class="fa-solid fa-trash-can fa-xs mail__sidebar-compose-popup-footer-icons-icon--right"></i>
+              <i className="fa-brands fa-google-drive fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
+              <i className="fa-regular fa-image fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
+              <i className="fa-solid fa-lock fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
+              <i className="fa-solid fa-pen fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
+              <i className="fa-solid fa-ellipsis-vertical fa-xs mail__sidebar-compose-popup-footer-icons-icon"></i>
+              <i className="fa-solid fa-trash-can fa-xs mail__sidebar-compose-popup-footer-icons-icon--right"></i>
             </div>
           </div>
         </div>
@@ -248,50 +351,11 @@ function MailComponent() {
 
 function ComposePopup({ onClose }) {
   const [subject, setSubject] = useState("");
-
-  return (
-    <div className="mail__sidebar-compose-popup">
-    <div className="mail__sidebar-compose-popup-header">
-      <h4 className="mail__sidebar-compose-popup-header-text">New Message</h4>
-      <div className="mail__sidebar-compose-popup-header-right">
-        <button className="mail__sidebar-compose-popup-header-button" onClick={() => alert('Minimize clicked')}>_</button>
-        <button className="mail__sidebar-compose-popup-header-button" onClick={closeComposePopup}>X</button>
-      </div>
-    </div>
-    
-    <div className="mail__sidebar-compose-popup-body">
-      <div className="mail__sidebar-compose-popup-body-from">
-        <label className="mail__sidebar-compose-popup-body-from-text">From</label>
-        <input className="mail__sidebar-compose-popup-body-from-input" type="text" placeholder="youremail@example.com" readOnly />
-      </div>
-
-      <div className="mail__sidebar-compose-popup-body-to">
-        <label className="mail__sidebar-compose-popup-body-to-text">To</label>
-        <input className="mail__sidebar-compose-popup-body-to-input" type="text" placeholder="Recipient's email" />
-      </div>
-      
-      <div className="mail__sidebar-compose-popup-body-subject">
-        <label className="mail__sidebar-compose-popup-body-subject-text">Subject</label>
-        <input className="mail__sidebar-compose-popup-body-subject-input" type="text" placeholder="Subject" />
-      </div>
-
-      <div className="mail__sidebar-compose-popup-body-text">
-        <textarea className="mail__sidebar-compose-popup-body-text-inside" placeholder="Message content..."></textarea>
-      </div>
-    </div>
-
-    <div className="mail__sidebar-compose-popup-footer">
-      <button className="mail__sidebar-compose-popup-footer-button">Send</button>
-
-      <div className="mail__sidebar-compose-popup-footer-icons">
-        <i className="fa fa-paperclip mail__sidebar-compose-popup-footer-icon"></i>
-        <i className="fa fa-image mail__sidebar-compose-popup-footer-icon"></i>
-        <i className="fa fa-smile mail__sidebar-compose-popup-footer-icon"></i>
-      </div>
-    </div>
-  </div>
-  );
 }
 
-// React bileşenini render et
-ReactDOM.render(<MailComponent />, document.getElementById('root'));
+// createRoot kullanarak root oluşturuyoruz
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+// Uygulamayı render ediyoruz
+root.render(<MailComponent />);
